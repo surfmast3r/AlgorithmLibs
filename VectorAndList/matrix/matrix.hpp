85d28a2180945202ba15774e996d04351a1aef70
surfmast3r <surfmast3r@live.it>
2021-05-26 16:12:32 +0200

    matrix interaface definition

--- VectorAndList/matrix/matrix.hpp
+++ VectorAndList/matrix/matrix.hpp
@@ -12,9 +12,9 @@ namespace lasd {
 
 /* ************************************************************************** */
 
-template <typename Data>
-class Matrix { // Must extend MappableContainer<Data> and FoldableContainer<Data>
-
+template <typename DataType>
+class Matrix : virtual public MappableContainer<DataType>,
+			   virtual public FoldableContainer<DataType>{ // Must extend MappableContainer<Data> and FoldableContainer<Data>
 private:
 
   // ...
@@ -26,36 +26,35 @@ protected:
 public:
 
   // Destructor
-  // ~Matrix() specifiers
-
+   virtual ~Matrix() = default;
   /* ************************************************************************ */
 
   // Copy assignment
-  // type operator=(argument); // Copy assignment of abstract types should not be possible.
+   Matrix& operator=(const Matrix&) noexcept = delete; // Copy assignment of abstract types should not be possible.
 
   // Move assignment
-  // type operator=(argument); // Move assignment of abstract types should not be possible.
+   Matrix& operator=(Matrix&&) noexcept = delete; // Move assignment of abstract types should not be possible.
 
   /* ************************************************************************ */
 
   // Comparison operators
-  // type operator==(argument) specifiers; // Comparison of abstract types might not be possible.
-  // type operator!=(argument) specifiers; // Comparison of abstract types might not be possible.
+   bool operator==(const Matrix&) const noexcept= delete; // Comparison of abstract types might not be possible.
+   bool operator!=(const Matrix&) const noexcept = delete; // Comparison of abstract types might not be possible.
 
   /* ************************************************************************ */
 
   // Specific member functions
 
-  // type RowNumber() specifiers; // (concrete function should not throw exceptions)
-  // type ColumnNumber() specifiers; // (concrete function should not throw exceptions)
+   virtual unsigned long& RowNumber() const noexcept = 0; // (concrete function should not throw exceptions)
+   virtual unsigned long& ColumnNumber() const noexcept = 0; // (concrete function should not throw exceptions)
 
-  // type RowResize() specifiers;
-  // type ColumnResize() specifiers;
+   virtual void RowResize() = 0;
+   virtual void ColumnResize() = 0;
 
-  // type ExistsCell() specifiers; // (concrete function should not throw exceptions)
+   virtual bool ExistsCell() const noexcept = 0; // (concrete function should not throw exceptions)
 
-  // type operator()() specifiers; // Mutable access to the element (concrete function should throw exceptions only when out of range)
-  // type operator()() specifiers; // Immutable access to the element (concrete function should throw exceptions when not present)
+   virtual DataType& operator()() = 0; // Mutable access to the element (concrete function should throw exceptions only when out of range)
+   virtual DataType const& operator()() const = 0; // Immutable access to the element (concrete function should throw exceptions when not present)
 
 };
 
